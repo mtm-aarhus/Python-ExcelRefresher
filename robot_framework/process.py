c8b8b70273449531cad8cbf235c5bfff6f646723
Laura-Salmonsen <balas@aarhus.dk>
2026-07-30 09:41:55 +0200

    Update process.py

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -381,8 +381,8 @@ def send_faktura_mail(local_file_path: str, file_name: str, orchestrator_connect
     msg = EmailMessage()
     msg["From"] = sender
     msg["To"] = recipients
-    # msg["Cc"] = orchestrator_connection.get_constant('SapFakturaHenterHRMail').value.rsplit(',')[1]
-    msg["Cc"] = orchestrator_connection.get_constant('balas').value
+    msg["Cc"] = orchestrator_connection.get_constant('SapFakturaHenterHRMail').value.rsplit(',')[1]
+    # msg["Cc"] = orchestrator_connection.get_constant('balas').value
     msg["Subject"] = subject
     msg.set_content("Aktiver HTML for at se denne besked.")
     msg.add_alternative(html_body, subtype="html")
